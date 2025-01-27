53963a3d76dfd0bcbb6558bd0bc26fa5226bf8ac
Noam Shani <Noams156@gmail.com>
2025-01-27 13:25:56 +0100

    flask installed & issues resolved

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
+# from flask import Flask
+
+# def create_app():
+#     """App factory function."""
+#     app = Flask(__name__)
+#     app.config.from_object('src.config.Config')
+
+#     # Register blueprints
+#     from src.routes.auth import auth_bp
+#     app.register_blueprint(auth_bp)
+
+#     return app
 from flask import Flask
 
-def create_app():
-    """App factory function."""
-    app = Flask(__name__)
-    app.config.from_object('src.config.Config')
+app = Flask(__name__)
 
-    # Register blueprints
-    from src.routes.auth import auth_bp
-    app.register_blueprint(auth_bp)
+@app.route('/')
+def home():
+    return "Hello, Flask!"
 
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
